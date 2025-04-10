567160660779862f7b1112a1ce8f06bd480dce92
Ajay <112794893+Ajay-308@users.noreply.github.com>
2025-04-10 23:00:31 +0530

    instant meet

---
+++ hooks/useMeetingActions.ts
@@ -0,0 +1,45 @@
+import { useRouter } from "next/router";
+import { useStreamVideoClient } from "@stream-io/video-react-sdk";
+import toast from "react-hot-toast";
+
+export const useMeetingActions = () => {
+  const router = useRouter();
+  const streamClient = useStreamVideoClient();
+  const createInstantMeeting = async () => {
+    if (!streamClient) {
+      toast.error("stream client not found");
+      return;
+    }
+
+    try {
+      const id = crypto.randomUUID();
+      const call = streamClient.call("default", id);
+      await call.getOrCreate({
+        data: {
+          starts_at: new Date().toISOString(),
+          custom: {
+            Description: "instant meeting",
+          },
+        },
+      });
+      router.push(`/meeting/${id}`);
+      toast.success("meeting created successfully");
+    } catch (e) {
+      console.log("error while creating instant meeting", e);
+      toast.error("error while crearting instant meeting");
+    }
+  };
+
+  const joinMeeting = async (callId: string) => {
+    if (!streamClient) {
+      toast.error("stream client not found");
+      return;
+    }
+
+    router.push(`/meeting/${callId}`);
+  };
+  return {
+    createInstantMeeting,
+    joinMeeting,
+  };
+};
